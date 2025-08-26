ee08815eb5aafa596129edf7c5b01bc6cab92f7e
Raphaelle Lacerda <raphaellelacerda.tds2.senai@gmail.com>
2025-08-26 11:19:36 -0300

    feat: create function dividir and multiplicar

--- src/calculadora.js
+++ src/calculadora.js
@@ -7,3 +7,13 @@ export function subtrair(a, b) {
     return a - b;
 }
 
+export function multiplicar(a, b){
+    return a * b;
+}
+
+export function dividir(a, b){
+    if(b == 0){
+        throw new Error('Divisão por zero não permitida');
+    }
+    return a / b;
+}
